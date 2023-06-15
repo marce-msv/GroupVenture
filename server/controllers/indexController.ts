ceef1bcf4348287e3b84b1c1fc65e2f206a8160b
alejandrop <geo.al3x@gmail.com>
2023-06-15 18:19:50 +0200

    clean: final version for sure

---
+++ server/controllers/indexController.ts
@@ -0,0 +1,6 @@
+import * as usersController from "./usersController";
+// @ts-ignore
+import * as activityController from "./activityController";
+import * as userActivityController from "./userActivityController";
+
+exports = { usersController, activityController, userActivityController };
