52e54d331acaeff62dc1f611fae1fe1672285fa6
alejandrop <geo.al3x@gmail.com>
2023-06-15 18:18:11 +0200

    clean: files removed

--- server/controllers/indexController.js
+++
@@ -1,9 +0,0 @@
-const usersController = require("./usersController");
-const activityController = require("./activityController");
-const userActivityController = require("./userActivityController");
-
-module.exports = {
-  usersController,
-  activityController,
-  userActivityController,
-};
