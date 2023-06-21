c9107a6e990026b531d774655d8b32b7747f0c9b
Marcelo Suarez <marcelo.suarez.v@gmail.com>
2023-06-21 14:42:27 +0200

    feat: E2E tests added

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -10,28 +10,68 @@ describe('home page', () => {
   });
 
   it('should go to login and back home', () => {
-    cy.get('#login-btn').click();
+    cy.contains('Login').click();
     cy.location('pathname').should('eq', '/login');
     cy.get('#email').type('perro@mail.com');
     cy.get('#password').type('123');
     cy.get('.ripple').click();
     cy.location('pathname').should('eq', '/profile/5');
     cy.get('.infoAboutUser').should('exist');
-    cy.get('#home-btn').click();
+    cy.contains('Home').click();
     cy.location('pathname').should('eq', '/');
   });
 
-  it('should be able to create a new user', () => {
+  it('should be able to create a new user and logout', () => {
     cy.get('[href="/signup"]').click();
     cy.location('pathname').should('eq', '/signup');
-    // cy.get('#avatar').selectFile('../public/cat.jpeg');
+    cy.contains('Sign Up Now');
+    cy.get('input[type=file]')
+      .invoke('show')
+      .selectFile('cypress/fixtures/cat.jpeg');
     cy.get('#firstName').type('Top');
     cy.get('#lastName').type('Cat');
     cy.get('#age').type('2');
-    cy.get('#email').type('topcat@mail.com');
+    cy.get('#email').type('i@mail.com'); // change every test
     cy.get('#password').type('123456');
     cy.get('#infoAboutUser').type('Leader of The Gang');
     cy.get('.ripple').click();
-    cy.location('pathname').should('eq', '/login'); // This route should be redirected to home
+    cy.get(':nth-child(4) > a').click();
+    cy.contains(
+      "Are you sure you want to log out? You won't be able to add an activities anymore"
+    );
+    cy.get('a > .ripple').click();
+    cy.location('pathname').should('eq', '/');
+  });
+
+  it('log in and create a new activity', () => {
+    const dayjs = require('dayjs');
+
+    cy.contains('Login').click();
+    cy.location('pathname').should('eq', '/login');
+    cy.get('#email').type('perro@mail.com');
+    cy.get('#password').type('123');
+    cy.get('.ripple').click();
+    cy.location('pathname').should('eq', '/profile/5');
+    cy.get('.infoAboutUser').should('exist');
+    cy.get('.btns > div > .ripple').click();
+    cy.contains('Add an activity');
+    cy.get('#title').type('random activity');
+    cy.get('#date').type(dayjs().format('YYYY-MM-DDThh:mm'));
+    cy.get('#meetingPoint').type('Pl. de Catalunya');
+    cy.get('#typeOfActivity').select('trip').should('have.value', 'trip');
+    cy.get('#spots').type(5);
+    cy.get('#telegramLink').type('telegram/link');
+    cy.get('#aboutActivity').type('Great activity');
+    cy.get('.ripple').click();
+    cy.location('pathname').should('eq', '/');
+  });
+
+  it('filter activities by city', () => {
+    cy.get('#meetingPoint').type('barcelona');
+    cy.get('.search-button').click();
+  });
+  it('filter activities by type', () => {
+    cy.get('#typeOfActivity').select('hiking').should('have.value', 'hiking');
+    cy.get('.search-button').click();
   });
 });
